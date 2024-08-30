30c1363e699c1e44dd7672254c6c62eb199313ca
WhackFather <147020181+whackfather@users.noreply.github.com>
2024-08-30 00:08:44 -0500

    Update main.py

--- binary to decimal/main.py
+++ binary to decimal/main.py
@@ -1,1 +1,13 @@
+# Binary to Decimal Converter
 
+from convert import converter
+
+print('Binary to Decimal Converter')
+
+while True:
+    initial = input('Binary number ("e" to exit): ')
+    if initial == "e":
+        print("Closing program")
+        exit(0)
+    else:
+        converter(initial)
