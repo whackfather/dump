9a686de935f63def9d1f5eb228b0a7aff6e34463
WhackFather <147020181+whackfather@users.noreply.github.com>
2024-10-23 13:25:34 -0500

    compaction

--- csci128 stuff/8_letters.py
+++ csci128 stuff/8_letters.py
@@ -10,50 +10,38 @@ def get_input_grids():
     grid = []
     for i in range(5):
         newRow = input(f"ROW{i + 1}> ")
-        if newRow == "DONE":
-            return -1
+        if newRow == "DONE": return -1
         grid.append(newRow.split())
-
     return grid
 
 def split_input_grids(inputGrids:list):
     letters = []
-    try:
-        x = inputGrids[0][0]
-    except:
-        return []
+    try: x = inputGrids[0][0]
+    except: return []
     amtLetters = (len(inputGrids[0]) // 6) + 1
     for i in range(len(inputGrids)):
         for j in range(len(inputGrids[i])):
-            if inputGrids[i][j] != "0" and inputGrids[i][j] != "1":
-                return -1
-    
+            if inputGrids[i][j] != "0" and inputGrids[i][j] != "1": return -1
     startInd = 0
     for i in range(amtLetters):
         oneLetter = []
         for row in inputGrids:
             letLine = row[startInd:(startInd + 6)]
-            for i in range(len(letLine)):
-                letLine[i] = int(letLine[i])
-            if len(letLine) > 5:
-                letLine.pop()
+            for i in range(len(letLine)): letLine[i] = int(letLine[i])
+            if len(letLine) > 5: letLine.pop()
             oneLetter.append(letLine)
         letters.append(oneLetter)
         startInd += 6
-
     return letters
 
 def compute_scores(templates, letter_grid):
     scores = []
-    toTry = len(templates)
     for trying in templates:
         score = 0
         for row in range(len(trying)):
             for col in range(len(trying[row])):
-                if trying[row][col] == letter_grid[row][col]:
-                    score += 1
+                if trying[row][col] == letter_grid[row][col]: score += 1
         scores.append(score)
-
     return scores
 
 def extract_letter(letters, computed_scores):
@@ -63,16 +51,13 @@ def extract_letter(letters, computed_scores):
         if computed_scores[score] > highest:
             highest = computed_scores[score]
             ind = score
-    
     return letters[ind]
 
 if __name__ == "__main__":
     word = ""
     userIn = get_input_grids()
-    if userIn == -1:
-        print("OUTPUT ERROR: Invalid grid entered")
-    elif split_input_grids(userIn) == -1:
-        print("OUTPUT ERROR: Invalid pixel value encountered")
+    if userIn == -1: print("OUTPUT ERROR: Invalid grid entered")
+    elif split_input_grids(userIn) == -1: print("OUTPUT ERROR: Invalid pixel value encountered")
     else:
         numLetters = len(split_input_grids(userIn))
         print(f"OUTPUT {numLetters} Letters entered")
